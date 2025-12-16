3e3ce796c7c2582b02e2e4f3ee992d7b83ce5a13
Jéssica Tizziani <118910295+jessicatizziani@users.noreply.github.com>
2025-12-16 13:56:15 +0000

    atualização 16/012/2025

--- app/dashboard.py
+++ app/dashboard.py
@@ -26,6 +26,24 @@ grafico = df.groupby("categoria")["valor"].sum().reset_index()
 st.bar_chart(grafico, x="categoria", y="valor")
 
 
+# Gráfico mensal
+fig = px.line(
+    df_mensal,
+    x="mes",
+    y="valor_total",
+    markers=True,
+    title="Evolução Mensal dos Gastos - 2025"
+)
+
+fig.update_layout(
+    xaxis_title="Mês",
+    yaxis_title="Valor Total (R$)",
+    template="plotly_white"
+)
+
+st.plotly_chart(fig, use_container_width=True)
+
+
 @st.cache_data(ttl=60)
 def carregar_dados():
     return pd.read_csv("data/seu_arquivo.csv")
